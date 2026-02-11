a3cee83506fd67abaa6dae49b097a1dc481ba9dc
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2026-02-11 12:39:55 +0100

    feat: use /simple endpoint for python search
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

--- client/src/app/pages/python-list/python-list.tsx
+++ client/src/app/pages/python-list/python-list.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { generatePath, useNavigate } from "react-router-dom";
+
+import dayjs from "dayjs";
 
 import {
   Bullseye,
@@ -8,6 +11,11 @@ import {
   CardHeader,
   ClipboardCopy,
   Content,
+  Divider,
+  EmptyState,
+  EmptyStateActions,
+  EmptyStateBody,
+  EmptyStateFooter,
   Flex,
   FlexItem,
   Icon,
@@ -28,9 +36,13 @@ import {
 } from "@patternfly/react-core";
 import CertificateIcon from "@patternfly/react-icons/dist/esm/icons/certificate-icon";
 import ClockIcon from "@patternfly/react-icons/dist/esm/icons/clock-icon";
+import CubesIcon from "@patternfly/react-icons/dist/esm/icons/cubes-icon";
 import UserIcon from "@patternfly/react-icons/dist/esm/icons/user-icon";
 
-import type { PythonPythonPackageContentResponse } from "@app/client";
+import type {
+  DistributionResponse,
+  PythonPythonPackageContentResponse,
+} from "@app/client";
 import { ConditionalDataListBody } from "@app/components/DataListControls/ConditionalDataListBody";
 import { DocumentMetadata } from "@app/components/DocumentMetadata";
 import { FilterToolbar, FilterType } from "@app/components/FilterToolbar";
@@ -41,17 +53,28 @@ import {
   useTableControlProps,
   useTableControlState,
 } from "@app/hooks/table-controls";
+import { useFetchDistributions } from "@app/queries/distributions";
 import { useFetchPackages } from "@app/queries/packages";
 import { Paths } from "@app/Routes";
 import { toCamelCase } from "@app/utils/utils";
-import dayjs from "dayjs";
-import { generatePath, useNavigate } from "react-router-dom";
+
+import { DistributionSelector } from "./components/DistributionsSelector";
 
 export const PythonList: React.FC = () => {
   const navigate = useNavigate();
 
+  // Context selector
+  const { distributions } = useFetchDistributions();
+  const [selectedDistribution, setSelectedDistribution] =
+    React.useState<DistributionResponse | null>(null);
+  const onDistributionSelected = (value: DistributionResponse) => {
+    setSelectedDistribution(value);
+  };
+
+  // Sorting
   const [isSortByOpen, setIsSortByOpen] = React.useState<boolean>(false);
 
+  // Table
   const tableControlState = useTableControlState({
     tableName: "python",
     persistenceKeyPrefix: TablePersistenceKeyPrefixes.python_wheels,
@@ -118,163 +141,203 @@ export const PythonList: React.FC = () => {
   return (
     <>
       <DocumentMetadata title={"Python"} />
-      <PageSection>
-        <Title headingLevel="h1" size="2xl">
-          Python List
-        </Title>
+      <PageSection type="breadcrumb">
+        <DistributionSelector
+          distributions={distributions}
+          selected={selectedDistribution}
+          onChange={onDistributionSelected}
+        />
       </PageSection>
       <PageSection>
-        <Stack>
-          <StackItem>
-            <Toolbar {...toolbarProps}>
-              <ToolbarContent>
-                <FilterToolbar showFiltersSideBySide {...filterToolbarProps} />
-                <ToolbarItem variant="separator" />
-                <ToolbarItem>
-                  <Select
-                    id="sort-by"
-                    isOpen={isSortByOpen}
-                    selected={activeSort?.columnKey}
-                    onSelect={(_e, value) => {
-                      setActiveSort({
-                        // biome-ignore lint/suspicious/noExplicitAny: allowed
-                        columnKey: value as any,
-                        direction: activeSort?.direction ?? "asc",
-                      });
-                    }}
-                    onOpenChange={(isOpen) => setIsSortByOpen(isOpen)}
-                    toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
-                      <MenuToggle
-                        ref={toggleRef}
-                        onClick={() => setIsSortByOpen(!isSortByOpen)}
-                        isExpanded={isSortByOpen}
-                        style={
-                          {
-                            width: "200px",
-                          } as React.CSSProperties
-                        }
+        <Divider />
+      </PageSection>
+      {selectedDistribution ? (
+        <>
+          <PageSection>
+            <Title headingLevel="h1" size="2xl">
+              Selected distribution
+            </Title>
+          </PageSection>
+          <PageSection>
+            <Stack>
+              <StackItem>
+                <Toolbar {...toolbarProps}>
+                  <ToolbarContent>
+                    <FilterToolbar
+                      showFiltersSideBySide
+                      {...filterToolbarProps}
+                    />
+                    <ToolbarItem variant="separator" />
+                    <ToolbarItem>
+                      <Select
+                        id="sort-by"
+                        isOpen={isSortByOpen}
+                        selected={activeSort?.columnKey}
+                        onSelect={(_e, value) => {
+                          setActiveSort({
+                            // biome-ignore lint/suspicious/noExplicitAny: allowed
+                            columnKey: value as any,
+                            direction: activeSort?.direction ?? "asc",
+                          });
+                        }}
+                        onOpenChange={(isOpen) => setIsSortByOpen(isOpen)}
+                        toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+                          <MenuToggle
+                            ref={toggleRef}
+                            onClick={() => setIsSortByOpen(!isSortByOpen)}
+                            isExpanded={isSortByOpen}
+                            style={
+                              {
+                                width: "200px",
+                              } as React.CSSProperties
+                            }
+                          >
+                            {toCamelCase(activeSort?.columnKey ?? "")}
+                          </MenuToggle>
+                        )}
+                        shouldFocusToggleOnSelect
                       >
-                        {toCamelCase(activeSort?.columnKey ?? "")}
-                      </MenuToggle>
-                    )}
-                    shouldFocusToggleOnSelect
+                        <SelectList>
+                          {sortableColumns?.map((e) => (
+                            <SelectOption key={e} value={e}>
+                              {toCamelCase(activeSort?.columnKey ?? "")}
+                            </SelectOption>
+                          ))}
+                        </SelectList>
+                      </Select>
+                    </ToolbarItem>
+                    <ToolbarItem {...paginationToolbarItemProps}>
+                      <SimplePagination
+                        idPrefix="python-list"
+                        isTop
+                        paginationProps={paginationProps}
+                      />
+                    </ToolbarItem>
+                  </ToolbarContent>
+                </Toolbar>
+              </StackItem>
+              <StackItem>
+                <Stack aria-label="python-list" hasGutter>
+                  <ConditionalDataListBody
+                    isLoading={isFetching}
+                    isError={!!fetchError}
+                    isNoData={totalItemCount === 0}
                   >
-                    <SelectList>
-                      {sortableColumns?.map((e) => (
-                        <SelectOption key={e} value={e}>
-                          {toCamelCase(activeSort?.columnKey ?? "")}
-                        </SelectOption>
-                      ))}
-                    </SelectList>
-                  </Select>
-                </ToolbarItem>
-                <ToolbarItem {...paginationToolbarItemProps}>
+                    {currentPageItems?.map((item, rowIndex) => {
+                      return (
+                        <StackItem
+                          key={`${item.name}-${item.version}`}
+                          aria-labelledby={`Item-${rowIndex}`}
+                        >
+                          <Card isCompact isClickable>
+                            <CardHeader
+                              selectableActions={{
+                                onClickAction: () => onClickCard(item),
+                                selectableActionAriaLabelledby: `${item.name}-card}`,
+                              }}
+                            >
+                              <Flex spaceItems={{ default: "spaceItemsSm" }}>
+                                <FlexItem>
+                                  <Content component="h4">{item.name}</Content>
+                                </FlexItem>
+                                <FlexItem>
+                                  <Label isCompact>{item.version}</Label>
+                                </FlexItem>
+                              </Flex>
+                            </CardHeader>
+                            <CardBody>
+                              <Content component="small">
+                                {item.summary}
+                              </Content>
+                            </CardBody>
+                            <CardFooter>
+                              <Flex spaceItems={{ default: "spaceItemsSm" }}>
+                                <FlexItem>
+                                  <Icon>
+                                    <ClockIcon />
+                                  </Icon>{" "}
+                                  {dayjs(item.pulp_last_updated).fromNow()}
+                                </FlexItem>
+                                <FlexItem>
+                                  <Icon>
+                                    <UserIcon />
+                                  </Icon>{" "}
+                                  <Truncate
+                                    maxCharsDisplayed={35}
+                                    content={
+                                      item.author ||
+                                      item.author_email ||
+                                      item.maintainer_email ||
+                                      "Unknown"
+                                    }
+                                  />
+                                </FlexItem>
+                                <FlexItem>
+                                  <Icon>
+                                    <CertificateIcon />
+                                  </Icon>{" "}
+                                  <Truncate
+                                    maxCharsDisplayed={35}
+                                    content={
+                                      item.license ||
+                                      item.license_expression ||
+                                      "Unknown"
+                                    }
+                                  />
+                                </FlexItem>
+                                <FlexItem align={{ default: "alignRight" }}>
+                                  <ClipboardCopy
+                                    isReadOnly
+                                    hoverTip="Copy"
+                                    clickTip="Copied"
+                                    variant="inline-compact"
+                                  >
+                                    pip install {item.name ?? ""}
+                                  </ClipboardCopy>
+                                </FlexItem>
+                              </Flex>
+                            </CardFooter>
+                          </Card>
+                        </StackItem>
+                      );
+                    })}
+                  </ConditionalDataListBody>
+                </Stack>
+              </StackItem>
+              <StackItem>
+                <Bullseye>
                   <SimplePagination
-                    idPrefix="python-table"
-                    isTop
+                    idPrefix="python-list"
+                    isTop={false}
                     paginationProps={paginationProps}
                   />
-                </ToolbarItem>
-              </ToolbarContent>
-            </Toolbar>
-          </StackItem>
-          <StackItem>
-            <Stack aria-label="Python list" hasGutter>
-              <ConditionalDataListBody
-                isLoading={isFetching}
-                isError={!!fetchError}
-                isNoData={totalItemCount === 0}
-              >
-                {currentPageItems?.map((item, rowIndex) => {
-                  return (
-                    <StackItem
-                      key={`${item.name}-${item.version}`}
-                      aria-labelledby={`Item-${rowIndex}`}
-                    >
-                      <Card isCompact isClickable>
-                        <CardHeader
-                          selectableActions={{
-                            onClickAction: () => onClickCard(item),
-                            selectableActionAriaLabelledby: `${item.name}-card}`,
-                          }}
-                        >
-                          <Flex spaceItems={{ default: "spaceItemsSm" }}>
-                            <FlexItem>
-                              <Content component="h4">{item.name}</Content>
-                            </FlexItem>
-                            <FlexItem>
-                              <Label isCompact>{item.version}</Label>
-                            </FlexItem>
-                          </Flex>
-                        </CardHeader>
-                        <CardBody>
-                          <Content component="small">{item.summary}</Content>
-                        </CardBody>
-                        <CardFooter>
-                          <Flex spaceItems={{ default: "spaceItemsSm" }}>
-                            <FlexItem>
-                              <Icon>
-                                <ClockIcon />
-                              </Icon>{" "}
-                              {dayjs(item.pulp_last_updated).fromNow()}
-                            </FlexItem>
-                            <FlexItem>
-                              <Icon>
-                                <UserIcon />
-                              </Icon>{" "}
-                              <Truncate
-                                maxCharsDisplayed={35}
-                                content={
-                                  item.author ||
-                                  item.author_email ||
-                                  item.maintainer_email ||
-                                  "Unknown"
-                                }
-                              />
-                            </FlexItem>
-                            <FlexItem>
-                              <Icon>
-                                <CertificateIcon />
-                              </Icon>{" "}
-                              <Truncate
-                                maxCharsDisplayed={35}
-                                content={
-                                  item.license ||
-                                  item.license_expression ||
-                                  "Unknown"
-                                }
-                              />
-                            </FlexItem>
-                            <FlexItem align={{ default: "alignRight" }}>
-                              <ClipboardCopy
-                                isReadOnly
-                                hoverTip="Copy"
-                                clickTip="Copied"
-                                variant="inline-compact"
-                              >
-                                pip install {item.name ?? ""}
-                              </ClipboardCopy>
-                            </FlexItem>
-                          </Flex>
-                        </CardFooter>
-                      </Card>
-                    </StackItem>
-                  );
-                })}
-              </ConditionalDataListBody>
+                </Bullseye>
+              </StackItem>
             </Stack>
-          </StackItem>
-          <StackItem>
-            <Bullseye>
-              <SimplePagination
-                idPrefix="python-list"
-                isTop={false}
-                paginationProps={paginationProps}
-              />
-            </Bullseye>
-          </StackItem>
-        </Stack>
-      </PageSection>
+          </PageSection>
+        </>
+      ) : (
+        <PageSection>
+          <EmptyState
+            titleText="Empty state"
+            headingLevel="h4"
+            icon={CubesIcon}
+          >
+            <EmptyStateBody>
+              Packages are grouped in distributions. Select one to see packages.
+            </EmptyStateBody>
+            <EmptyStateFooter>
+              <EmptyStateActions>
+                <DistributionSelector
+                  distributions={distributions}
+                  selected={selectedDistribution}
+                  onChange={onDistributionSelected}
+                />
+              </EmptyStateActions>
+            </EmptyStateFooter>
+          </EmptyState>
+        </PageSection>
+      )}
     </>
   );
 };
